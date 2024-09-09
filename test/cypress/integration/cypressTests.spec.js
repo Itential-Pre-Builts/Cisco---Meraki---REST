226a32b448f86446165fd6e7d224cc32d84cc369
chauvo5297 <chau.vo@itential.com>
2024-09-09 16:29:21 -0400

    Add project file, update cypress tests and metadata

--- test/cypress/integration/cypressTests.spec.js
+++ test/cypress/integration/cypressTests.spec.js
@@ -1,4 +1,4 @@
-import { WorkflowRunner, PrebuiltRunner } from '@itential-tools/iap-cypress-testing-library/testRunner/testRunners';
+import { WorkflowRunner, PrebuiltRunner, ProjectRunner } from '@itential-tools/iap-cypress-testing-library/testRunner/testRunners';
 const CreateNetworkMerakiRESTJob3Data = require('../fixtures/stubs/Create Network - Meraki - REST Job3.json');
 const ClaimNetworkDevicesMerakiRESTJob4Data = require('../fixtures/stubs/Claim Network Devices - Meraki - REST Job4.json');
 const CreateNetworkMerakiRESTJob5Data = require('../fixtures/stubs/Create Network - Meraki - REST Job5.json');
@@ -37,6 +37,15 @@ function initializeWorkflowRunner(workflow, importWorkflow, isStub, stubTasks) {
   return workflowRunner;
 }
 
+// Helper function to find workflow to test from Project
+function findWorkflowInProject(workflowName, project) {
+  for (const projectComponent of project.components) {
+    if (projectComponent.type === 'workflow' && projectComponent.document.name === workflowName) {
+      return projectComponent.document;
+    }
+  }
+}
+
 // Function to delete the stubbed workflow and reimport it without the stub tasks
 function replaceStubTasks(workflowRunner, workflowName) {
     workflowRunner.deleteWorkflow.allCopies({
@@ -47,7 +56,7 @@ function replaceStubTasks(workflowRunner, workflowName) {
     workflowRunner.verifyWorkflow.hasNoDuplicates({});
 }
 
-describe('Default: Cypress Tests', function () {
+describe('Default: Pre-Built Cypress Tests', function () {
   let prebuiltRunner;
   let CreateNetworkandClaimDevicesMerakiExampleJob0Workflow;
   let CreateNetworkandClaimDevicesMerakiExampleJob1Workflow;
@@ -58,7 +67,7 @@ describe('Default: Cypress Tests', function () {
   let ClaimNetworkDevicesMerakiRESTJob6Workflow;
   
   before(function () {
-    //creates a prebuilt runner for importing the project
+    //creates a Pre-Built runner for importing the Pre-Built
     cy.fixture(`../../../artifact.json`).then((data) => {
       prebuiltRunner = new PrebuiltRunner(data);
     });
@@ -81,9 +90,9 @@ describe('Default: Cypress Tests', function () {
     prebuiltRunner.deletePrebuilt.single({ failOnStatusCode: false });
   });
 
-  describe('Default: Imports Project', function () {
+  describe('Default: Imports Pre-Built', function () {
     // eslint-disable-next-line mocha/no-hooks-for-single-case
-    it('Default: Should import the project into IAP', function () {
+    it('Default: Should import the Pre-Built into IAP', function () {
         prebuiltRunner.deletePrebuilt.single({ failOnStatusCode: false });
         prebuiltRunner.importPrebuilt.single({});
     });
@@ -181,6 +190,132 @@ describe('Default: Cypress Tests', function () {
         });
         /* Restore the workflow without the stub tasks */
         replaceStubTasks(workflowRunner, ClaimNetworkDevicesMerakiRESTJob6Workflow);
+      });
+    })
+  })
+});
+
+describe('Default: Project Cypress Tests', function () {
+  let projectRunner;
+  let projectData;
+
+  before(function () {
+    // Creates a Project runner for importing the Project
+    cy.fixture(`../../../Cisco - Meraki - REST.project.json`).then((data) => {
+      projectData = data;
+      projectRunner = new ProjectRunner(data);
+    });
+  });
+
+  after(function() {
+    projectRunner.deleteProject.single({ failOnStatusCode: false });
+  });
+
+  describe('Default: Imports Project', function () {
+    // eslint-disable-next-line mocha/no-hooks-for-single-case
+    it('Default: Should import the project into IAP', function () {
+        projectRunner.deleteProject.single({ failOnStatusCode: false });
+        projectRunner.importProject.single({});
+        projectRunner.verifyProject.exists();
+    });
+  })
+
+  describe('Create Network - Meraki - REST', function() {
+    it('Create Network - Meraki - REST: It should create a new network in Meraki and claim devices without tags', function () {
+      const workflowData = findWorkflowInProject('Create Network - Meraki - REST', projectData);
+      const importWorkflow = true;
+      const isStub = true;
+      // create the job runner so it can be used in future tests
+      const workflowRunner = initializeWorkflowRunner(workflowData, importWorkflow, isStub, CreateNetworkMerakiRESTJob3Data.stubTasks);
+      // this has to be customized to each IAP version.
+
+      workflowRunner.job.startAndReturnResultsWhenComplete({
+        options: CreateNetworkMerakiRESTJob3Data.input,
+        retryTime: 2000,
+      }).then((jobVariableResults) => {
+        expect(jobVariableResults['status']).eql(CreateNetworkMerakiRESTJob3Data.expectedTaskResults.status);
+        workflowRunner.job.getJobVariables(jobVariableResults._id).then(jobVariables => {
+          delete jobVariables._id;
+          delete jobVariables.initiator;
+          expect(jobVariables).eql(CreateNetworkMerakiRESTJob3Data.expectedTaskResults.variables);
+        });
+        /* Restore the workflow without the stub tasks */
+        replaceStubTasks(workflowRunner, workflowData);
+      });
+    })
+  })
+
+  describe('Claim Network Devices - Meraki - REST', function() {
+    it('Claim Network Devices - Meraki - REST: It should create a new network in Meraki and claim devices without tags', function () {
+      const workflowData = findWorkflowInProject('Claim Network Devices - Meraki - REST', projectData);
+      const importWorkflow = true;
+      const isStub = true;
+      // create the job runner so it can be used in future tests
+      const workflowRunner = initializeWorkflowRunner(workflowData, importWorkflow, isStub, ClaimNetworkDevicesMerakiRESTJob4Data.stubTasks);
+      // this has to be customized to each IAP version.
+
+      workflowRunner.job.startAndReturnResultsWhenComplete({
+        options: ClaimNetworkDevicesMerakiRESTJob4Data.input,
+        retryTime: 2000,
+      }).then((jobVariableResults) => {
+        expect(jobVariableResults['status']).eql(ClaimNetworkDevicesMerakiRESTJob4Data.expectedTaskResults.status);
+        workflowRunner.job.getJobVariables(jobVariableResults._id).then(jobVariables => {
+          delete jobVariables._id;
+          delete jobVariables.initiator;
+          expect(jobVariables).eql(ClaimNetworkDevicesMerakiRESTJob4Data.expectedTaskResults.variables);
+        });
+        /* Restore the workflow without the stub tasks */
+        replaceStubTasks(workflowRunner, workflowData);
+      });
+    })
+  })
+
+  describe('Create Network - Meraki - REST', function() {
+    it('Create Network - Meraki - REST: It should create a new network in Meraki and claim devices with tags', function () {
+      const workflowData = findWorkflowInProject('Create Network - Meraki - REST', projectData);
+      const importWorkflow = true;
+      const isStub = true;
+      // create the job runner so it can be used in future tests
+      const workflowRunner = initializeWorkflowRunner(workflowData, importWorkflow, isStub, CreateNetworkMerakiRESTJob5Data.stubTasks);
+      // this has to be customized to each IAP version.
+
+      workflowRunner.job.startAndReturnResultsWhenComplete({
+        options: CreateNetworkMerakiRESTJob5Data.input,
+        retryTime: 2000,
+      }).then((jobVariableResults) => {
+        expect(jobVariableResults['status']).eql(CreateNetworkMerakiRESTJob5Data.expectedTaskResults.status);
+        workflowRunner.job.getJobVariables(jobVariableResults._id).then(jobVariables => {
+          delete jobVariables._id;
+          delete jobVariables.initiator;
+          expect(jobVariables).eql(CreateNetworkMerakiRESTJob5Data.expectedTaskResults.variables);
+        });
+        /* Restore the workflow without the stub tasks */
+        replaceStubTasks(workflowRunner, workflowData);
+      });
+    })
+  })
+
+  describe('Claim Network Devices - Meraki - REST', function() {
+    it('Claim Network Devices - Meraki - REST: It should create a new network in Meraki and claim devices with tags', function () {
+      const workflowData = findWorkflowInProject('Claim Network Devices - Meraki - REST', projectData);
+      const importWorkflow = true;
+      const isStub = true;
+      // create the job runner so it can be used in future tests
+      const workflowRunner = initializeWorkflowRunner(workflowData, importWorkflow, isStub, ClaimNetworkDevicesMerakiRESTJob6Data.stubTasks);
+      // this has to be customized to each IAP version.
+
+      workflowRunner.job.startAndReturnResultsWhenComplete({
+        options: ClaimNetworkDevicesMerakiRESTJob6Data.input,
+        retryTime: 2000,
+      }).then((jobVariableResults) => {
+        expect(jobVariableResults['status']).eql(ClaimNetworkDevicesMerakiRESTJob6Data.expectedTaskResults.status);
+        workflowRunner.job.getJobVariables(jobVariableResults._id).then(jobVariables => {
+          delete jobVariables._id;
+          delete jobVariables.initiator;
+          expect(jobVariables).eql(ClaimNetworkDevicesMerakiRESTJob6Data.expectedTaskResults.variables);
+        });
+        /* Restore the workflow without the stub tasks */
+        replaceStubTasks(workflowRunner, workflowData);
       });
     })
   })
